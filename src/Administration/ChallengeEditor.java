a9041994ce40e6b4eee055e2ffa76583f81c10ec
solala5 <wendl.max@gmail.com>
2017-01-02 22:30:05 +0100

    Edited randomizer and challenge

--- src/Administration/ChallengeEditor.java
+++ src/Administration/ChallengeEditor.java
@@ -1,9 +1,24 @@
 package Administration;
 
+import Challenges.Challenge;
 /**
  * Created by Max on 02.01.2017.
  */
 public class ChallengeEditor {
+
+    public ChallengeEditor(int challengeID){
+        Challenge challengeToEdit = getChallengeFromID(challengeID);
+    }
+
+    private Challenge getChallengeFromID(int challengeID){
+        //Establish a connection to the DataBase and get the Challenge behind a specific ID
+        return
+
+    }
+
+
+
+
 
 
 }
