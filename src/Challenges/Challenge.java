9a6b4cb6af4218950da20e246e912006f82cd5e1
solala5 <wendl.max@gmail.com>
2017-01-02 20:37:43 +0100

    Edited Challenge.java

--- src/Challenges/Challenge.java
+++ src/Challenges/Challenge.java
@@ -6,11 +6,64 @@ import java.sql.Time;
  * Created by Max on 02.01.2017.
  */
 public class Challenge {
+
     String title;
     String description;
-    Time CompletionTime;
+    int CompletionTime;
     int idCreator;
     int idChallenge;
     int idChallenged;
-    int cooleIDvonDerChallengeSoVong1UpdateHer;
+    int vote;
+
+    public Challenge(){
+        vote=0;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String description(){
+        return description;
+    }
+    public int CompletionTime(){
+        return CompletionTime;
+    }
+
+    public int getCreatorId(){
+        return idCreator;
+    }
+
+    public int getChallengeId() {
+        return idChallenge;
+    }
+    public int getChallengendId() {
+        return idChallenged;
+    }
+    public int getVote() {
+        return vote;
+    }
+    public void setTitle(String setTitle) {
+        title = setTitle;
+    }
+    public void setDescription(String setDescription) {
+        description = setDescription;
+    }
+    public void setCompletionTime(int setCompletionTime) {
+        CompletionTime = setCompletionTime;
+    }
+    public void setCreatorId(int setCreatorId) {
+        idCreator = setCreatorId;
+    }
+    public void setChallengeId(int setChallengeId) {
+        idChallenge = setChallengeId;
+    }
+    public void setChallengedId(int setChallengedId) {
+        idChallenged = setChallengedId;
+    }
+
+    public void userVote(int newVote){
+        vote += newVote;
+    }
 }
+
+
