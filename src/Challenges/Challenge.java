078c832b1d09bb2d1ce253b09ddf13c53f0e1ac6
solala5 <wendl.max@gmail.com>
2017-01-02 19:40:02 +0100

    testttttt

--- src/Challenges/Challenge.java
+++ src/Challenges/Challenge.java
@@ -1,7 +1,15 @@
 package Challenges;
 
+import java.sql.Time;
+
 /**
  * Created by Max on 02.01.2017.
  */
 public class Challenge {
+    String title;
+    String description;
+    Time CompletionTime;
+    int idCreator;
+    int idChallenge;
+    int id
 }
