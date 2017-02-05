a50214c2417f5927d6ea106b7981474f27eb6db4
FLX-K <felix.kiessling@fh-erfurt.de>
2017-02-05 13:07:56 +0100

    Revert "Revert "Added UserData Constructor""
    This reverts commit 109cee751df48b5383a990ebe5d7da03cf68db76.

--- src/Profile/Registration.java
+++ src/Profile/Registration.java
@@ -1,16 +1,19 @@
 package Profile;
 
+
+import java.io.PrintWriter;
+
 /**
  * Created by Max on 02.01.2017.
  */
 public class Registration {
     public static UserData register(String name, String password, int day, int month, int year) {
         UserData user = new UserData();
-        user.username = name;
-        user.birthday = day;
-        user.birthmonth = month;
-        user.birthyear = year;
-        user.password = password;
+        user.setName(user, name);
+        user.setDay(user, day);
+        user.setMonth(user, month);
+        user.setyear(user, year);
+        user.setPass(user, password);
 
         /* get next free id from database */
 
