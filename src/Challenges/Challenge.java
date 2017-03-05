f5bfb20dc806cb391f3dde2968f7f446a0c7d2d8
FLX-K <felix.kiessling@fh-erfurt.de>
2017-03-05 13:28:08 +0100

    Challenge javadocs

--- src/Challenges/Challenge.java
+++ src/Challenges/Challenge.java
@@ -15,13 +15,16 @@ public class Challenge {
     private int idChallenge;
     private int idChallenged;
     private int vote;
+    connectDataBase database;
 
-    public Challenge(){
-        vote=0;
-    }
-
-    public Challenge(String title, String description, int completionTime, int idCreator){
-        connectDataBase database = new connectDataBase();
+    /**
+     * Creates a new challenge and inserts it into the database
+     * @param title The title of the challenge
+     * @param description The challenge description
+     * @param completionTime The time to complete the challenge in seconds
+     * @param idCreator User-ID of user creating the challenge
+     */
+    public Challenge(String title, String description, int completionTime, int idCreator) {
         this.title = title;
         this.description = description;
         this.completionTime = completionTime;
@@ -29,73 +32,136 @@ public class Challenge {
         idChallenge = getNewChallengeID();
         idChallenged = 0;
         vote = 0;
+        database = new connectDataBase();
         String insertString = "INSERT INTO challenges (ChallengeID, challenged, creator, title, description, completionTime, votes) VALUES("
                 + idChallenge + ", " + idChallenged + ", " + idCreator + ", '" + title + "', '" + description + "', " + completionTime + ", " + vote + ")";
         database.insertQery(insertString);
     }
 
+    /**
+     * Starts the countdown for the challenge
+     */
     public void startChallenge(){
         timerListener listener = new timerListener(completionTime);
         Timer timer = new Timer(1000, listener);
         timer.start();
     }
 
+    /**
+     * Gets the next free ID from the challenge database
+     * @return Free challenge ID
+     */
     public int getNewChallengeID(){
         int NextFreeIDFromDatabase = 0;
+        String query = "SELECT * FROM challenges UP TO 1 ROWS ORDER BY challengeid DESCENDING";
+        database.insertQery(query);
         return NextFreeIDFromDatabase;
     }
 
+    /**
+     * Get the title of the challenge
+     * @return The title of the challenge
+     */
     public String getTitle(){
         return title;
     }
-    public String description(){
-        return description;
-    }
-    public int CompletionTime(){
-        return completionTime;
-    }
 
-    public int getCreatorId(){
-        return idCreator;
-    }
-
-    public int getChallengeId() {
-        return idChallenge;
-    }
-    public int getChallengendId() {
-        return idChallenged;
-    }
-    public int getVote() {
-        return vote;
-    }
-
+    /**
+     * Get the description of the challenge
+     * @return The description of the challenge
+     */
     public String getDescription(){
         return description;
     }
 
+    /**
+     * Get the challenge creator's ID
+     * @return The challenge creator's ID
+     */
+    public int getCreatorId(){ return idCreator; }
+
+    /**
+     * Get the challenge's ID
+     * @return The challenge's ID
+     */
+    public int getChallengeId() {
+        return idChallenge;
+    }
+
+    /**
+     * Get the challenged person's user ID
+     * @return The challenged person's user ID
+     */
+    public int getChallengendId() { return idChallenged; }
+
+    /**
+     * Get the current voting of the challenge
+     * @return The current voting of the challenge
+     */
+    public int getVote() {
+        return vote;
+    }
+
+    /**
+     * Get remaining completion time
+     * @return The remaining completion time
+     */
     public int getCompletionTime(){
         return completionTime;
     }
 
+    /**
+     * Set the title of the challenge
+     * @param setTitle The new title
+     */
     public void setTitle(String setTitle) {
         title = setTitle;
     }
+
+    /**
+     * Set the description of the title
+     * @param setDescription The new description
+     */
     public void setDescription(String setDescription) {
         description = setDescription;
     }
+
+    /**
+     * Set the completion time
+     * @param setCompletionTime The new completion time in seconds
+     */
     public void setCompletionTime(int setCompletionTime) {
         completionTime = setCompletionTime;
     }
+
+    /**
+     * Set the challenge's creator ID
+     * @param setCreatorId The challenge creator's user ID
+     */
     public void setCreatorId(int setCreatorId) {
         idCreator = setCreatorId;
     }
+
+    /**
+     * Set the challenge ID
+     * @param setChallengeId The new challenge ID
+     */
     public void setChallengeId(int setChallengeId) {
         idChallenge = setChallengeId;
     }
+
+    /**
+     * Set the challenged user's ID
+     * @param setChallengedId The challenged user's ID
+     */
     public void setChallengedId(int setChallengedId) {
         idChallenged = setChallengedId;
     }
 
+    /**
+     * Vote the challenge
+     * @param value The value to add (1 for positive, -1 for negative vote)
+     */
     public void userVote(int value) {
         vote += value;
     }
